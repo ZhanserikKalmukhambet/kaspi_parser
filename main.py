c55ba92c192af90b054d3dd0f09577d6dcf7f4c5
zhanserikKalmukhambet <zhanserik.kalmukhambet@gmail.com>
2024-08-12 12:50:22 +0500

    updated version

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import logging
 import csv
-import pandas as pd
 
 from datetime import datetime, timedelta
 from client import KaspiClient
 from convert import convert_datetime_to_milliseconds as conv_d
 from convert import convert_datetime_from_milliseconds as conv_d_rev
 
+from decorators import measure_time
+
 connector = KaspiClient()
 
 logger = logging.getLogger(__name__)
 
-file_name = 'sman_orders_archive'
 
-
-def write_to_excel_file(file=f'{file_name}.csv'):
-    df = pd.read_csv(file, delimiter=';')
-
-    excel_file = f'{file_name}.xlsx'
-    df.to_excel(excel_file, index=False)
-
-
-def write_to_csv_file(data: list):
+def write_to_csv_file(data: list, file_name: str):
     with open(f'{file_name}.csv', 'a') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow(data)
@@ -37,31 +29,37 @@ def parse(page_number: int, start_date: int, end_date: int):
                  'last_name': d['attributes']['customer']['lastName'],
                  'date': conv_d_rev(d['attributes']['creationDate'])}
 
-        write_to_csv_file([order['id'], order['first_name'], order['last_name'], order['phone_number'], order['date']])
+        write_to_csv_file([order['id'], order['first_name'], order['last_name'], order['phone_number'], order['date']],
+                          file_name=file_name)
 
 
-def func():
-    start_d, end_d = '2020-02-28', '2024-05-11'
-
+@measure_time
+def func(start_d: str, end_d: str, file_name: str):
     start_date = datetime.strptime(start_d, '%Y-%m-%d')
     end_date = datetime.strptime(end_d, '%Y-%m-%d')
 
-    write_to_csv_file(['Номер заказа', 'Телефон', 'Имя клиента', 'Фамилия клиента', 'Дата заказа'])
+    write_to_csv_file(data=['Номер заказа', 'Телефон', 'Имя клиента', 'Фамилия клиента', 'Дата заказа'],
+                      file_name=file_name)
 
     while start_date < end_date:
         next_date = start_date + timedelta(days=14)
 
         # calc
-        pageCnt = connector.get_page_cnt_of_orders(start_milliseconds=conv_d(start_date),
-                                                   end_milliseconds=conv_d(next_date))
+        page_cnt = connector.get_page_cnt_of_orders(start_milliseconds=conv_d(start_date),
+                                                    end_milliseconds=conv_d(next_date))
 
-        print('parsing dates between: %s and %s. pageCnt = %s' % (start_date, next_date, pageCnt))
+        print('parsing dates between: %s and %s. pageCnt = %s' % (start_date, next_date, page_cnt))
 
-        for page in range(pageCnt):
+        for page in range(page_cnt + 1):
             parse(page_number=page, start_date=conv_d(start_date), end_date=conv_d(next_date))
 
         start_date = next_date
 
 
 if __name__ == '__main__':
-    func()
+    start_d = input('Start date in format %Y-%M-%D: ')
+    end_d = input('End date in format %Y-%M-%D: ')
+
+    file_name = input('Output file name: ')
+
+    func(start_d=start_d, end_d=end_d, file_name=file_name)
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-
 from convert import convert_datetime_to_milliseconds as conv_d
 
-start_d, end_d = '2024-04-28', '2024-05-11'
+start_d = input('Start date in format %Y-%M-%D: ')
+end_d = input('End date in format %Y-%M-%D: ')
 
 start_date = datetime.strptime(start_d, '%Y-%m-%d')
 end_date = datetime.strptime(end_d, '%Y-%m-%d')
